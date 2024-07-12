ccf7576d41ce5b1b0739b073bba7fd85f4e5c560
LionelMazet <mazet@bmpowersystem.fr>
2024-07-12 15:11:17 +0200

    Creation du premier commut

---
+++ BlinkLED/main.c
@@ -0,0 +1,30 @@
+
+
+/**
+ * main.c
+ */
+#include <device.h>
+#include <driverlib.h>
+int main(void)
+{
+    Device_init();
+    Device_initGPIO();
+
+    GPIO_setPadConfig(DEVICE_GPIO_PIN_LED1, GPIO_PIN_TYPE_STD);
+    GPIO_setDirectionMode(DEVICE_GPIO_PIN_LED1, GPIO_DIR_MODE_OUT);
+
+    Interrupt_initModule();
+    Interrupt_initVectorTable();
+
+    EINT;
+    ERTM;
+
+    for(;;) {
+        GPIO_writePin(DEVICE_GPIO_PIN_LED1, 0);
+        DEVICE_DELAY_US(500000);
+        GPIO_writePin(DEVICE_GPIO_PIN_LED1, 1);
+        DEVICE_DELAY_US(500000);
+    }
+
+	return 0;
+}
